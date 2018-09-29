9ac9468b1528871e53632d4310ab0ee2ef2231ec
David Holroyd <dave@badgers-in-foil.co.uk>
2018-09-29 23:56:52 +0100

    Parse all elementary streams in benchmark
    Processing the audio too should not make much difference, but there's
    not particular reason to only handle the video

--- benches/bench.rs
+++ benches/bench.rs
@@ -8,7 +8,6 @@ use std::fs::File;
 use std::io::Read;
 use mpeg2ts_reader::demultiplex;
 use mpeg2ts_reader::pes;
-use mpeg2ts_reader::StreamType;
 
 packet_filter_switch!{
     NullFilterSwitch<NullDemuxContext> {
@@ -28,8 +27,7 @@ impl demultiplex::StreamConstructor for NullStreamConstructor {
         match req {
             demultiplex::FilterRequest::ByPid(0) => NullFilterSwitch::Pat(demultiplex::PatPacketFilter::new()),
             demultiplex::FilterRequest::ByPid(_) => NullFilterSwitch::Null(demultiplex::NullPacketFilter::new()),
-            demultiplex::FilterRequest::ByStream(StreamType::H264, pmt_section, stream_info) => NullElementaryStreamConsumer::construct(pmt_section, stream_info),
-            demultiplex::FilterRequest::ByStream(_stype, _pmt_section, _stream_info) => NullFilterSwitch::Null(demultiplex::NullPacketFilter::new()),
+            demultiplex::FilterRequest::ByStream(_stream_type, pmt_section, stream_info) => NullElementaryStreamConsumer::construct(pmt_section, stream_info),
             demultiplex::FilterRequest::Pmt{pid, program_number} => NullFilterSwitch::Pmt(demultiplex::PmtPacketFilter::new(pid, program_number)),
             demultiplex::FilterRequest::Nit{..} => NullFilterSwitch::Null(demultiplex::NullPacketFilter::new()),
         }
@@ -46,7 +44,15 @@ impl NullElementaryStreamConsumer {
 }
 impl pes::ElementaryStreamConsumer for NullElementaryStreamConsumer {
     fn start_stream(&mut self) { println!("start_steam()"); }
-    fn begin_packet(&mut self, _header: pes::PesHeader) { }
+    fn begin_packet(&mut self, header: pes::PesHeader) {
+        if let pes::PesContents::Parsed(Some(content)) = header.contents() {
+            match content.pts_dts() {
+                Ok(pes::PtsDts::PtsOnly(Ok(ts))) => { criterion::black_box(ts); },
+                Ok(pes::PtsDts::Both{ pts: Ok(ts), .. }) => { criterion::black_box(ts); },
+                _ => (),
+            };
+        }
+    }
     fn continue_packet(&mut self, _data: &[u8]) { }
     fn end_packet(&mut self) { }
     fn continuity_error(&mut self) { }
