b962ffd4286f6e200e5a624d4075242379bef6f2
David Holroyd <dave@badgers-in-foil.co.uk>
2022-12-03 00:44:00 +0000

    Avoid failure testing bench code in CI

--- benches/bench.rs
+++ benches/bench.rs
@@ -74,16 +74,26 @@ impl<Ctx> pes::ElementaryStreamConsumer<Ctx> for NullElementaryStreamConsumer {
 }
 
 fn mpeg2ts_reader(c: &mut Criterion) {
-    let mut f = File::open("resources/testsrc.ts")
-        .expect("Test file missing.  To create, run: mkdir -p resources && ffmpeg -f lavfi -i testsrc=duration=20:size=640x360:rate=30,noise=alls=20:allf=t+u -f lavfi -i sine=duration=20:frequency=1:beep_factor=480:sample_rate=48000 -c:v libx264 -b:v 20M -map 0:v -c:a aac -b:a 128k -map 1:a -vf format=yuv420p -f mpegts resources/testsrc.ts");
-    let l = f.metadata().unwrap().len() as usize;
-    let size = l.min(188 * 200_000);
-    let mut buf = vec![0; size];
-    f.read(&mut buf[..]).unwrap();
+    let buf = if std::env::var("GITHUB_ACTIONS").is_ok() || std::env::var("GIT_AUTHOR_DATE").is_ok()
+    {
+        // HACK
+        // we don't run benchmarking on github, or in pre-commit hook, but we do test that the
+        // benchmark code executes without errors, so we need an input buffer of some kind for the
+        // code to consume
+        vec![0; 188]
+    } else {
+        let mut f = File::open("resources/testsrc.ts")
+            .expect("Test file missing.  To create, run: mkdir -p resources && ffmpeg -f lavfi -i testsrc=duration=20:size=640x360:rate=30,noise=alls=20:allf=t+u -f lavfi -i sine=duration=20:frequency=1:beep_factor=480:sample_rate=48000 -c:v libx264 -b:v 20M -map 0:v -c:a aac -b:a 128k -map 1:a -vf format=yuv420p -f mpegts resources/testsrc.ts");
+        let l = f.metadata().unwrap().len() as usize;
+        let size = l.min(188 * 200_000);
+        let mut buf = vec![0; size];
+        f.read(&mut buf[..]).unwrap();
+        buf
+    };
     let mut ctx = NullDemuxContext::new();
     let mut demux = demultiplex::Demultiplex::new(&mut ctx);
     let mut group = c.benchmark_group("parse");
-    group.throughput(Throughput::Bytes(size as _));
+    group.throughput(Throughput::Bytes(buf.len() as _));
     group.bench_function("parse", move |b| {
         b.iter(|| demux.push(&mut ctx, &buf[..]));
     });
